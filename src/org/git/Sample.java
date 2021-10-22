70af350d3763dd47e3935ee2f64df1aa402776e0
RamahAcnt <ramahravi11@gmail.com>
2021-10-22 11:48:38 +0530

    ramahthrd

--- src/org/git/Sample.java
+++ src/org/git/Sample.java
@@ -1,9 +1,16 @@
 package org.git;
 
 public class Sample {
+	
+	private void add(int a , int b) {
+		
+		int c= a+b;
+		System.out.println(c);
+
+	}
 
 	public static void main(String[] args) {
-		System.out.println("This is a sample ");
-		
+		Sample s = new Sample();
+		s.add(10, 25);
 	}
 }
